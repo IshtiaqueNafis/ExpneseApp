ea3a36932ab688d4bb119f9de28ff7843a389066
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-01-28 12:06:02 -0500

    added expense type model and added expense property in data ApplicationDBContext class

--- ExpneseApp/Data/ApplicationDBContext.cs
+++ ExpneseApp/Data/ApplicationDBContext.cs
@@ -6,6 +6,7 @@ namespace ExpneseApp.Data
     public class ApplicationDBContext:DbContext // inertied so the database can be created
     {
         public DbSet<Expense> Expenses { get; set; } // this the expense property created 
+        public DbSet<ExpenseType> ExpenseTypes { get; set; } // this the expense property created 
 
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext>options):base(options)
         { 
---
+++ ExpneseApp/Models/ExpenseType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpneseApp.Models
+{
+    public class ExpenseType
+    {
+      [Key]  public int Id { get; set; }
+      public string ExpenseCategory { get; set; }
+    }
+}

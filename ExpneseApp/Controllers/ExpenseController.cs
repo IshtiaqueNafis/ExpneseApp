09b82fc9b2364c7b1a63ba253501199d50292cf5
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-01-27 19:48:36 -0500

    added expense controller

---
+++ ExpneseApp/Controllers/ExpenseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpneseApp.Data;
+
+namespace ExpneseApp.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public ExpenseController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var objList = _db.Expenses; // getting the expense table
+            return View(objList);
+        }
+    }
+}
